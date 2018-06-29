5a098d52d0e71a64ae65fbc1ec6e0b7e77c27410
Simon Gregg <sbgregg@wolftech.ad.ncsu.edu>
2018-06-29 15:09:30 -0400

    runoff estimation clarifications additional analysis

--- hydro.ana_180508.R
+++ hydro.ana_180508.R
@@ -30,6 +30,53 @@ require("ggmap")        # Plotting of maps same as you would with ggplot2
 require("maptools")     # Read, write, and handle Shapefiles in R
 require("mapdata")      # Supplement to maps package
 
+##Begin user defined functions###########################################
+## For Runoff estimation using SCS CN method
+# Inlet1 runoff estimation function
+runoff.in1 <- function(acc, CN, WA = 17.72) { 
+  # convert accumulation to inches
+  acc.in <- acc * 3.94
+  # surface storage
+  S <- (1000/CN) - 10
+  # runoff units: inches
+  Q <- ((acc.in - (0.2 * S))^2)/(acc.in + (0.8 * S))
+  # conversion to volume: cubic feet
+  Q.vol <- Q * WA * (43560 / 12)
+  # conversion to cubic meters
+  Q.vol <- Q.vol * 0.0283
+  return(Q.vol)
+}
+
+# Inlet12 runoff estimation function
+runoff.in2 <- function(acc, CN, WA = 30.59) { 
+  # convert accumulation to inches
+  acc.in <- acc * 3.94
+  # surface storage
+  S <- (1000/CN) - 10
+  # runoff units: inches
+  Q <- ((acc.in - (0.2 * S))^2)/(acc.in + (0.8 * S))
+  # conversion to volume: cubic feet
+  Q.vol <- Q * WA * (43560 / 12)
+  # conversion to cubic meters
+  Q.vol <- Q.vol * 0.0283
+  return(Q.vol)
+}
+
+# Runon runoff estimation function
+runoff.runon <- function(acc, CN, WA = 1.95) { 
+  # convert accumulation to inches
+  acc.in <- acc * 3.94
+  # surface storage
+  S <- (1000/CN) - 10
+  # runoff units: inches
+  Q <- ((acc.in - (0.2 * S))^2)/(acc.in + (0.8 * S))
+  # conversion to volume: cubic feet
+  Q.vol <- Q * WA * (43560 / 12)
+  # conversion to cubic meters
+  Q.vol <- Q.vol * 0.0283
+  return(Q.vol)
+}
+######################### End
 ## Read file from ./Working folder
 ## units feet
 DS <- read.csv("./Working/dataset.csv")
@@ -49,10 +96,9 @@ DS.rain.metric <- DS %>%
             select("timestamp",
                    "rain.in") %>%
             transmute(timestamp = timestamp,
-                      rainfall = rain.in * 25.4,
-                      int.5min = (DS.rain.metric$rainfall + lag(DS.rain.metric$rainfall) + 
-                                    (0.5*(apply(cbind((lag(DS.rain.metric$rainfall, 2)), lead(DS.rain.metric$rainfall)), 1, max))))*12
-            )
+                      rainfall.mm = rain.in * 25.4,
+                      int.5min = (rainfall.mm + lag(rainfall.mm) + 
+                                    (0.5*(apply(cbind((lag(rainfall.mm, 2)), lead(rainfall.mm)), 1, max))))*12)
 # View(DS.rain.metric)
 
 ## Combine rainfall with flow data set
@@ -61,7 +107,7 @@ RSC.hydro.m <- left_join(DS.rain.metric, DS.flow, by = "timestamp")
 ## Remove extra position column
 RSC.hydro.m <- (RSC.hydro.m) %>%
                 select(timestamp,
-                       rainfall,
+                       rainfall.mm,
                        int.5min,
                        in1.m_flow,
                        dryout.m_flow,
@@ -73,12 +119,12 @@ RSC.hydro.m <- (RSC.hydro.m) %>%
 ## Remove extra data; only rain and time stamp for delineation
 RSC.rain <- (RSC.hydro.m) %>%
   select(timestamp,
-         rainfall)
+         rainfall.mm)
 # View(RSC.rain)
 
 ## Rainfall event delineation
 # Exstract from Drizzle0.9.5
-depth <- RSC.rain$rainfall
+depth <- RSC.rain$rainfall.mm
 depth[depth == 0] <- NA
 nalocf <- function(x) na.locf(x, maxgap = 359, na.rm = FALSE)
 rain.index <- cumsum(diff(!is.na(c(NA, nalocf(depth)))) > 0) + nalocf(0*depth)
@@ -108,6 +154,20 @@ RSC.hydro.m$storm.index <- (RSC.hydro.m$storm.index) %>%
                             replace_na(0)
 # View(RSC.hydro.m)
 
+## Create a flow dataset for viewing
+DS.flow.tot <- (DS.hydro.metric) %>%
+  select(timestamp, 
+         in1.m_flow,
+         in2.hobo.m_flow,
+         out.flow.roll.ASABE)
+## Melt inflow Dataset 
+DS.flow.tot <- (DS.flow.tot) %>%
+  melt(id = "timestamp")
+#View(DS.inflow)
+## Plot inFlow
+ggplot(DS.flow.tot, aes(x = timestamp))+
+  geom_line(aes(y = value, colour = variable))
+
 ## Split into list of events
 RainEvents <- split(RSC.hydro.m, RSC.hydro.m$storm.index) 
 # Returns a list of events 
@@ -117,11 +177,14 @@ RainEvents <- split(RSC.hydro.m, RSC.hydro.m$storm.index)
 # Returns a data frame of values same length as list
 Rainsum <- RainEvents %>%
   map_df(function(df) {summarise(df, Duration = ((max(timestamp)-min(timestamp))/3600),
-                                 Accumulation = sum(rainfall),
-                                 max.intensity5 = max(int.5min),
+                                 Accumulation = sum(rainfall.mm, na.rm = TRUE),
+                                 max.intensity5 = max(int.5min, na.rm = TRUE),
+                                 runoff.est.in1 = runoff.in1(Accumulation, CN = 86),
                                  in1.vol = sum(in1.m_flow, na.rm = TRUE) * (Duration * 3600),
                                  dryout.vol = sum(dryout.m_flow, na.rm = TRUE) * (Duration * 3600),
+                                 runoff.est.in2 = runoff.in2(Accumulation, CN = 84),
                                  in2.vol = sum(in2.m_flow, na.rm = TRUE) * (Duration * 3600),
+                                 runoff.est.runon = runoff.runon(Accumulation, CN = 87),
                                  out.vol = sum(out.flow.roll.ASABE, na.rm = TRUE) * (Duration * 3600),
-                                 in.sum = (in1.vol + in2.vol))})
+                                 in.sum = (in1.vol + in2.vol + runoff.est.runon))})
 # View(Rainsum)
