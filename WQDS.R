496b4de2516b79a0198ae5b235cb785564c5c515
Simon Gregg <sbgregg@wolftech.ad.ncsu.edu>
2018-07-03 10:16:52 -0400

    intial commit WQ analysis

---
+++ WQDS.R
@@ -0,0 +1,31 @@
+## Analysis of Morrisville RSC WQ data from Providance Farm, Mason Farm Road
+## monitoring period July 2017-June 2018
+## data location in ./Working
+## dataset preparation + analysis
+
+## Visualizing data
+require("ggplot2")      # Powerful and aesthetic plotting system for R
+require("gridExtra")    # Arrange multiple ggplots in same figure (multi-panels)
+require("scales")       #
+require("RColorBrewer") # creates nice color schemes
+require("corrplot")     # A graphical display of a correlation matrix between all combinations of variables
+## Statistical analysis
+require("stats")        # Lots of stats stuff
+## Data management
+require("plyr")         # Allows you t split data structure into groups (pollutant type, location, etc.) and apply function on each group
+require("dplyr")
+require("zoo")          # Helps streamline data if you have irregular time series
+require("reshape2")     # Convert data with "wide" columns to "long" columns
+require("lubridate")    # Date and time data made easy! See reference PDF in Google Drive
+require("data.table")
+require("TTR")
+#require("xlsx")        # creates errors # Reads and writes to xlsx file
+require("purrr")
+require("tidyr")
+require("fBasics")
+require("pls")
+## Mapping tools
+require("stringi")
+require("ggmap")        # Plotting of maps same as you would with ggplot2
+require("maptools")     # Read, write, and handle Shapefiles in R
+require("mapdata")      # Supplement to maps package
